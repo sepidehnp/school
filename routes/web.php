a5006785f978c61beaa86e86a632644842ac5e4a
sepideh <sepidehnp.67@gmail.com>
2025-05-24 19:35:40 +0330

    seesion 17

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,9 @@ Route::group(['prefix'=>'admin'],function(){
         Route::get('announcement/create', [AnnouncementController::class,'index'])->name('announcement.create');
         Route::post('announcement/store', [AnnouncementController::class,'store'])->name('announcement.store');
         Route::get('announcement/read', [AnnouncementController::class,'read'])->name('announcement.read');
+        Route::get('announcement/edit/{id}', [AnnouncementController::class,'edit'])->name('announcement.edit');
+        Route::post('announcement/update/{id}', [AnnouncementController::class,'update'])->name('announcement.update');
+        Route::get('announcement/delete/{id}', [AnnouncementController::class,'delete'])->name('announcement.delete');
 
         //class managment
         Route::get('class/create', [ClassesController::class,'index'])->name('class.create');
